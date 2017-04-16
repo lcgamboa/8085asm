2c4e97307d3319553d659faf4d87bdbd0835d0f7
lcgamboa <lcgamboa@yahoo.com>
2017-04-16 11:12:44 -0300

    Readme update

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# 8085asm - Simple 8085 assembler and disassembler
+
+Simple 8085 assembler and disassembler for didatic use.
+The 8085asm generate the map file and intel hex file. 
+Support the directives EQU, ORG, DB, DS and END.
+
+The 8085toZ80 application is a traslator of 8085asm(8080) for z80asm.  
+
+
---
+++ examples/Chaves_z80.asm
@@ -0,0 +1,15 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;Chaves.asm - Utilizacao de perifericos
+;Prof. Roberto M. Ziller - 04.01.2000
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+COMMAND EQU 20H
+CHAVES  EQU 21H
+LEDS    EQU 22H
+        ORG 2000H
+	LD A, 02H                    ;        MVI A,02H     ; Porta A como entrada, B como saida
+	OUT (COMMAND),A              ;        OUT COMMAND
+LOOP:	IN A, (CHAVES)          ;LOOP:   IN CHAVES     ; Le chaves para o acumulador
+	OUT (LEDS),A                 ;        OUT LEDS      ; Acende os LEDs correspondentes
+	JP LOOP                      ;        JMP LOOP
+	RET                          ;        RET
+        END
